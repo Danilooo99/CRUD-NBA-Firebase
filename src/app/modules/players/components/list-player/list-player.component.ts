45d0578ba607997984af3e9918d8bbb5577c7941
Danilooo99 <danilo.informatica.99@gmail.com>
2022-05-06 14:43:12 +0100

    Paginator of Table and mesaages of create and delete

--- src/app/modules/players/components/list-player/list-player.component.ts
+++ src/app/modules/players/components/list-player/list-player.component.ts
@@ -3,58 +3,67 @@ import { Player } from '../../models/player.model';
 import { PlayerService } from '../../services/player.service';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-list-player',
   templateUrl: './list-player.component.html',
   styleUrls: ['./list-player.component.css']
 })
-export class ListPlayerComponent implements OnInit/*, AfterViewInit*/ {
+export class ListPlayerComponent implements OnInit, AfterViewInit {
 
   dataSend: string = "Añadir nuevo jugador"; 
-  Players: Player[];
+  Players!: Player[];
 
-  //@ViewChild(MatPaginator) paginator: MatPaginator;
-  //dataSource: MatTableDataSource<Player>;
-  
-  constructor(private playerService: PlayerService) {
-   }
+  displayedColumns: string[] = ['name', 'surname', 'age', 'team', 'position', 'value', 'action'];
+ 
+  dataSource = new MatTableDataSource<Player>();
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(private playerService: PlayerService) {} 
 
   ngOnInit(): void {
-    this.playerService.getPlayers().subscribe( (res) => {
-      this.Players = res.map( (e) => {
-        return {
-          id: e.payload.doc.id,
-          ...(e.payload.doc.data() as Player)
-        }
+      this.playerService.getPlayers().subscribe( (res) => {
+          this.Players = res.map( (e) => {
+            return {
+              id: e.payload.doc.id,
+              ...(e.payload.doc.data() as Player)
+            }
+          });
       });
-    }); 
+    
+      this.playerService.getPlayers().subscribe( (res) => {
+        this.paginatorData(this.Players);
+      }); 
+
+      if(localStorage.getItem("confirm")!==null){
+        const error = document.getElementById("error");
+        const confirm = document.getElementById('confirm');
+        confirm.innerHTML = `<p>${localStorage.getItem("confirm")}</p>`;
+        localStorage.clear();
+        error.innerHTML="";
+      }
   }
 
-  /*ngAfterViewInit() {
-    console.log("Bro",this.dataSource);
+  ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
-  }*/
+  }
 
   deleteRow (player){
+    const confirm = document.getElementById('confirm');
     const error = document.getElementById("error");
     this.playerService.deletePlayer(player);
     error.innerHTML = `<p>Se ha eliminado con éxito al jugador ${player.name} ${player.surname}</p>`;
+    confirm.innerHTML="";
   } 
 
-  /*loadPlayersPaginator(){
-    this.playerService.getPlayers().subscribe( (res) => {
-      this.Players = res.map( (e) => {
-        this.dataSource = new MatTableDataSource(this.Players); 
-        this.dataSource.paginator = this.paginator;
-        return {
-          id: e.payload.doc.id,
-          ...(e.payload.doc.data() as Player)
-        }
-      });
-    });  
-  }*/
+  paginatorData(Players){
+    this.dataSource = new MatTableDataSource(Players);
+    this.dataSource.paginator = this.paginator;
+  }
 
-  displayedColumns: string[] = ['name', 'surname', 'age', 'team', 'position', 'value', 'action'];
-
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
